2c347ab967c6b943a47bbf6e34f7e79570117c93
Maira Bello <maira.araujo@liferay.com>
2015-06-04 09:57:19 -0300

    Implements gulp plugin for gulp-babel-deps

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 'use strict';
 
-module.exports = function() {
+var babelDeps = require('babel-deps');
+var gutil = require('gulp-util');
+var sourceMap  = require('vinyl-sourcemaps-apply');
+var through = require('through2');
 
+module.exports = function(options) {
+  var files = [];
+  var basePath;
+  function receiveFile(file, encoding, callback) {
+    basePath = file.base;
+    files.push({
+      contents: file.contents.toString(encoding),
+      options: {filename: file.path}
+    });
+    callback();
+  }
+
+  function compile(callback) {
+    try {
+      var results = babelDeps(files, options);
+      for (var i = 0; i < results.length; i++) {
+        var file = new gutil.File({
+          base: basePath,
+          contents: results[i].babel.code,
+          path: results[i].path
+        });
+        sourceMap(file, results[i].babel.map);
+        this.push(file); // jshint ignore:line
+      }
+      callback();
+    } catch (error) {
+      this.emit('error', error);
+      callback();
+      return;
+    }
+  }
+
+  return through.obj(receiveFile, compile);
 };
